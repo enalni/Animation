271d60990123d6962368c445284cd5af18baa626
Alexander <Alexander>
2020-03-24 23:36:13 +0300

    complete

---
+++ Animation/Animation.swift
@@ -0,0 +1,72 @@
+//
+//  Animation.swift
+//  Animation
+//
+//  Created by Александр Николаевич on 24.03.2020.
+//  Copyright © 2020 Alexander Nikolaevich. All rights reserved.
+//
+
+import Spring
+
+let animations: [Spring.AnimationPreset] = [
+    .Shake,
+    .Pop,
+    .Morph,
+    .Squeeze,
+    .Wobble,
+    .Swing,
+    .FlipX,
+    .FlipY,
+    .Fall,
+    .SqueezeLeft,
+    .SqueezeRight,
+    .SqueezeDown,
+    .SqueezeUp,
+    .SlideLeft,
+    .SlideRight,
+    .SlideDown,
+    .SlideUp,
+    .FadeIn,
+    .FadeOut,
+    .FadeInLeft,
+    .FadeInRight,
+    .FadeInDown,
+    .FadeInUp,
+    .ZoomIn,
+    .ZoomOut,
+    .Flash
+]
+
+var animationCurves: [Spring.AnimationCurve] = [
+    .EaseIn,
+    .EaseOut,
+    .EaseInOut,
+    .Linear,
+    .Spring,
+    .EaseInSine,
+    .EaseOutSine,
+    .EaseInOutSine,
+    .EaseInQuad,
+    .EaseOutQuad,
+    .EaseInOutQuad,
+    .EaseInCubic,
+    .EaseOutCubic,
+    .EaseInOutCubic,
+    .EaseInQuart,
+    .EaseOutQuart,
+    .EaseInOutQuart,
+    .EaseInQuint,
+    .EaseOutQuint,
+    .EaseInOutQuint,
+    .EaseInExpo,
+    .EaseOutExpo,
+    .EaseInOutExpo,
+    .EaseInCirc,
+    .EaseOutCirc,
+    .EaseInOutCirc,
+    .EaseInBack,
+    .EaseOutBack,
+    .EaseInOutBack
+]
+
+
--- Animation/ViewController.swift
+++ Animation/ViewController.swift
@@ -6,15 +6,36 @@
 //  Copyright © 2020 Alexander Nikolaevich. All rights reserved.
 //
 
-import UIKit
+import Spring
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var buttonSpring: SpringButton!
+    @IBOutlet weak var viewSpring: SpringView!
+    @IBOutlet weak var labelTextInView: UILabel!
+    
+    var text = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        buttonSpring.setTitle("GO", for: .normal)
+        labelTextInView.text = "WELCOME"
+        labelTextInView.font.withSize(CGFloat(50))
     }
+    
+    @IBAction func getAnimationSpring(_ sender: SpringButton) {
+        
+        viewSpring.animation = animations.randomElement()!.rawValue
+        viewSpring.curve = animationCurves.randomElement()!.rawValue
+        viewSpring.duration = CGFloat((Double.random(in: 0.95..<1)))
+        viewSpring.animate()
+        
 
-
+        text = "type animation - \(viewSpring.animation)"
+        labelTextInView.text = text
+        
+        
+        sender.setTitle("This is \(viewSpring.animation) animation" , for: .normal)
+        
+    }
 }
-
